3a24502a6caae6392574443892c770e39969d729
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-07-14 03:39:11 +0900

    diff test at linux

--- Test.py
+++ Test.py
@@ -7,15 +7,15 @@ st = time.time()
 env = gym.make('mouseCl-v0')
 env.seed(300)
 env.reset()
-# diff = 0
-# for _ in trange(100):
-#     diff += env.check_step(env.action_space.sample())
-for _ in trange(10000):
-    o, r, d, i = env.step(env.action_space.sample())
-    if d :
-        env.reset()
+diff = 0
+for _ in trange(100):
+    diff += env.check_step(env.action_space.sample())
+# for _ in trange(10000):
+#     o, r, d, i = env.step(env.action_space.sample())
+#     if d :
+#         env.reset()
 # env.render()
     # env.render()
-# print(diff)
+print(diff)
 # input('done:')
 print(time.time() - st)
